9536b0c95ef10f590f9222344e28e8597429e586
Phillip <artlyhub@gmail.com>
2017-11-20 22:35:04 +0900

    added home

--- artdb/views.py
+++ artdb/views.py
@@ -1,3 +1,6 @@
+from django.views import View
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html', {})
--- artly/urls.py
+++ artly/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from artdb.views import home
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('artdb.api.urls', namespace='api')),
+    url(r'^$', home, name='home'),
 ]
