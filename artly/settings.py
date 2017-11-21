c0a5ba605ef9673267ca3b7ad2bee415632c4524
Phillip <artlyhub@gmail.com>
2017-11-22 01:15:49 +0900

    media root

--- artly/settings.py
+++ artly/settings.py
@@ -145,6 +145,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = '/home/artly/artly.kr/media/'
 
 FILE_UPLOAD_MAX_MEMORY_SIZE = 200000000
